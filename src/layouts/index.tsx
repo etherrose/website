3d91e4d68005c2cfdd667219aea312725236b700
Huw McNamara <huwmcnamara@msn.com>
2018-06-25 19:04:10 +0100

    I updated everything

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,60 +1,69 @@
-import * as React from 'react';
-import Link from 'gatsby-link';
+import React from 'react';
 import Helmet from 'react-helmet';
 import { slide as Menu } from 'react-burger-menu';
-import { Search } from 'material-ui-icons';
+import { Search } from '@material-ui/icons';
 
-import '../../node_modules/foundation-sites/dist/css/foundation.css'
+import '../../node_modules/foundation-sites/dist/css/foundation.css';
 import './index.scss';
 
-interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
+interface IDefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
     location: {
-        pathname: string,
+        pathname: string;
     };
     children: any;
 }
 
-class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
+class DefaultLayout extends React.PureComponent<IDefaultLayoutProps, void> {
     public render() {
         return (
-            <div id='outer-container'>
-                <div className='grid-x'>
-                    <div className='cell medium-2'>
+            <div id="outer-container">
+                <div className="grid-x">
+                    <div className="cell medium-2">
                         <Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
-                            <a id='home' className='menu-item' href='/'>Home</a>
-                            <a id='about' className='menu-item menu-item--about' href='/about'>About</a>
-                            <a id='consultants' className='menu-item menu-item--sub' href='/about'>Consultants</a>
-                            <a id='projects' className='menu-item menu-item--sub' href='/about'>Projects</a>
-                            <a id='testimonials' className='menu-item menu-item--sub' href='/about'>Testimonials</a>
-                            <a id='contact' className='menu-item' href='/contact'>Contact</a>
+                            <a id="home" className="menu-item" href="/">
+                                Home
+                            </a>
+                            <a id="about" className="menu-item menu-item--about" href="/about">
+                                About
+                            </a>
+                            <a id="consultants" className="menu-item menu-item--sub" href="/about">
+                                Consultants
+                            </a>
+                            <a id="projects" className="menu-item menu-item--sub" href="/about">
+                                Projects
+                            </a>
+                            <a id="testimonials" className="menu-item menu-item--sub" href="/about">
+                                Testimonials
+                            </a>
+                            <a id="contact" className="menu-item" href="/contact">
+                                Contact
+                            </a>
                         </Menu>
                     </div>
                     <Helmet
-                        title='Ether Rose'
+                        title="Ether Rose"
                         meta={[
                             { name: 'description', content: 'Sample' },
                             { name: 'keywords', content: 'sample, something' },
                         ]}
                     />
-                    <div className='medium-10'>
-                        <main id='page-wrap'>
-                            <div className='grid-x'>
-                                <div className='cell medium-8'>
-                                    {this.props.children()}
-                                </div>
-                                <div className='cell medium-4'>
-                                    <div className='search-bar'>
-                                        <input type='text' placeholder='search' />
-                                        <button className="button search-bar--icon" type="sumbit"> <Search /></button>
+                    <div className="medium-10">
+                        <main id="page-wrap">
+                            <div className="grid-x">
+                                <div className="cell medium-8">{this.props.children()}</div>
+                                <div className="cell medium-4">
+                                    <div className="search-bar">
+                                        <input type="text" placeholder="search" />
+                                        <button className="button search-bar--icon" type="submit">
+                                            {' '}
+                                            <Search />
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </main>
-
                     </div>
                 </div>
-
-
             </div>
         );
     }
